f4c6ee18d7d1eb7a39ed9eb2f2bd7203f4870537
David Pucheta <dpucheta@easyexpunctions.com>
2018-08-10 12:48:35 -0500

    Header and Footer

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,62 +1,59 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
-
-function RenderDish({dish}) {
-    return(
-        <div  className="col-12 col-md-5 m-1">
-        <Card>
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
-            <CardBody>
-                    <CardTitle>
-                        {dish.name}
-                    </CardTitle>
-                    <CardText>{dish.description}</CardText>
-            </CardBody>
-        </Card>
+function RenderDish({ dish }) {
+  return (
+    <div className="col-12 col-md-5 m-1">
+      <Card>
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
     </div>
-    );
+  );
 }
 
-function RenderComments({comments}) {
-    console.dir(comments);
-    return(
-        <div className="col-12 col-md-5 m-1">
-        <h4>Comments</h4>
-        {comments.map(function(comment){
-            return(
-            <div key={comment.date}>
-                <div>{comment.comment}</div>
-                <div>--{comment.author}, &nbsp; 
-                    {new Intl.DateTimeFormat('en-US', { 
-                    year: 'numeric', 
-                    month: 'short', 
-                    day: '2-digit' 
-                    }).format(new Date(comment.date))}
-                </div>
+function RenderComments({ comments }) {
+  return (
+    <div className="col-12 col-md-5 m-1">
+      <h4>Comments</h4>
+      {comments.map(function(comment) {
+        return (
+          <div key={comment.date}>
+            <div>{comment.comment}</div>
+            <div>
+              --
+              {comment.author}, &nbsp;
+              {new Intl.DateTimeFormat("en-US", {
+                year: "numeric",
+                month: "short",
+                day: "2-digit"
+              }).format(new Date(comment.date))}
+            </div>
             <br />
-        </div>
-        )})}
-    </div>
-    );
-}
-
-const DishDetail = (props) => {
-const dish = props.dish;
-    if(dish != null){   
-        return(
-            <div className="container">
-                <div className="row">                 
-                    <RenderDish dish={dish}/>
-                   <RenderComments comments={dish.comments} />
-                </div>     
-           </div>         
+          </div>
         );
-    }
-    else {
-        return(<div></div>);
-    }
+      })}
+    </div>
+  );
 }
 
+const DishDetail = props => {
+  const dish = props.dish;
+  if (dish != null) {
+    return (
+      <div className="container">
+        <div className="row">
+          <RenderDish dish={dish} />
+          <RenderComments comments={dish.comments} />
+        </div>
+      </div>
+    );
+  } else {
+    return <div />;
+  }
+};
 
 export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,41 +1,43 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from  'reactstrap';
-import Menu from './MenuComponent';
-import DishDetail  from './DishDetailComponent';
-import { DISHES } from '../shared/dishes';
+import React, { Component } from "react";
+import Menu from "./MenuComponent";
+import DishDetail from "./DishDetailComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import { DISHES } from "../shared/dishes";
 
 class Main extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      dishes: DISHES,
+      selectedDish: null 
+    };
+  }
 
-        this.state = {
-            dishes: DISHES,
-            selectedDish: null
-        };
-
-    }
-
-    onDishSelect(dishId) {
-        this.setState({
-            selectedDish: dishId
-        });
-    }
+  onDishSelect(dishId) {
+    this.setState({
+      selectedDish: dishId
+    });
+  }
 
   render() {
     return (
       <div>
-        <Navbar dark color="primary" >
-          <div className="container">
-            <NavbarBrand href="/">
-              Ristorante con Fusion
-            </NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={ this.state.dishes }
-           onClick={ (dishId) => this.onDishSelect(dishId) } />
+        <Header />
+        <Menu
+          dishes={this.state.dishes}
+          onClick={dishId => this.onDishSelect(dishId)}
+        />
 
-           <DishDetail dish={ this.state.dishes.filter( (dish) => dish.id === this.state.selectedDish)[0] }/>
+        <DishDetail
+          dish={
+            this.state.dishes.filter(
+              dish => dish.id === this.state.selectedDish
+            )[0]
+          }
+        />
+        <Footer/>
       </div>
     );
   }
