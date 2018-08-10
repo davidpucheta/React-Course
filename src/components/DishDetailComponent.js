6904e7a12d06f281296c0eb7f43bcde400fc6c97
David Pucheta <dpucheta@easyexpunctions.com>
2018-08-10 11:07:41 -0500

    Lifecycle methods part 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -2,47 +2,53 @@ import React, { Component }  from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class DishDetail extends Component {
+    componentDidMount() {
+        console.log('Dishdetail componentDidMount invoked');
+    }
 
-    constructor(props){
-        super(props);
+    componentDidUpdate(){
+        console.log('Dishdetail componentDidUpdate invoked');
     }
 
     render() {
+
+        console.log('Dishdetail render() invoked');
+
         const dish = this.props.dish;
         if(dish != null){
                 return(
                     <div className="container">
-                    <div className="row">
-                        <div  className="col-12 col-md-5 m-1">
-                            <Card>
-                                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                                <CardBody>
-                                        <CardTitle>
-                                            {dish.name}
-                                        </CardTitle>
-                                        <CardText>{dish.description}</CardText>
-                                </CardBody>
-                            </Card>
-                        </div>
-
-                        <div className="col-12 col-md-5 m-1">
-                            <h4>Comments</h4>
-                            {dish.comments.map(function(comment){
-                                return(
-                                <div>
-                                    <div>{comment.comment}</div>
-                                    <div>--{comment.author}, &nbsp; 
-                                        {new Intl.DateTimeFormat('en-US', { 
-                                        year: 'numeric', 
-                                        month: 'short', 
-                                        day: '2-digit' 
-                                        }).format(new Date(comment.date))}
-                                    </div>
-                                <br />
+                        <div className="row">                 
+                            <div  className="col-12 col-md-5 m-1">
+                                <Card>
+                                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                                    <CardBody>
+                                            <CardTitle>
+                                                {dish.name}
+                                            </CardTitle>
+                                            <CardText>{dish.description}</CardText>
+                                    </CardBody>
+                                </Card>
                             </div>
-                            )})}
+
+                            <div className="col-12 col-md-5 m-1">
+                                <h4>Comments</h4>
+                                {dish.comments.map(function(comment){
+                                    return(
+                                    <div key={comment.date}>
+                                        <div>{comment.comment}</div>
+                                        <div>--{comment.author}, &nbsp; 
+                                            {new Intl.DateTimeFormat('en-US', { 
+                                            year: 'numeric', 
+                                            month: 'short', 
+                                            day: '2-digit' 
+                                            }).format(new Date(comment.date))}
+                                        </div>
+                                    <br />
+                                </div>
+                                )})}
+                            </div>
                         </div>
-                    </div>
                     </div>
                 );
         }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 
 class Menu extends Component {
 
     constructor(props){
         super(props);
+        console.log('Menu constructor invoked');
+    }
+
+    componentDidMount() {
+        console.log('Menu componentDidMount invoked');
     }
 
     render() {
@@ -15,7 +20,7 @@ class Menu extends Component {
                 <div key={dish.id} className="col-12 col-md-5 m-1"> 
                     <Card onClick={ () => this.props.onClick(dish.id) }>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardImgOverlay body className="ml-5">
+                        <CardImgOverlay body="true" className="ml-5">
                             <CardTitle>
                                 {dish.name}
                             </CardTitle>
