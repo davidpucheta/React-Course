7658c972fa0174f8784ef36fca96db3c7c8a0677
David Pucheta <dpucheta@easyexpunctions.com>
2018-08-13 16:35:35 -0500

    Single Page Applications Part 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Link } from 'react-router-dom'; 
 
 function RenderDish({ dish }) {
   return (
@@ -16,28 +17,32 @@ function RenderDish({ dish }) {
 }
 
 function RenderComments({ comments }) {
-  return (
-    <div className="col-12 col-md-5 m-1">
-      <h4>Comments</h4>
-      {comments.map(function(comment) {
-        return (
-          <div key={comment.date}>
-            <div>{comment.comment}</div>
-            <div>
-              --
-              {comment.author}, &nbsp;
-              {new Intl.DateTimeFormat("en-US", {
-                year: "numeric",
-                month: "short",
-                day: "2-digit"
-              }).format(new Date(comment.date))}
+ if (comments != null) {
+    return (
+      <div className="col-12 col-md-5 m-1">
+        <h4>Comments</h4>
+        {comments.map(function(comment) {
+          return (
+            <div key={comment.date}>
+              <div>{comment.comment}</div>
+              <div>
+                --
+                {comment.author}, &nbsp;
+                {new Intl.DateTimeFormat("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "2-digit"
+                }).format(new Date(comment.date))}
+              </div>
+              <br />
             </div>
-            <br />
-          </div>
-        );
-      })}
-    </div>
-  );
+          );
+        })}
+      </div>
+    );
+  } else {
+    return(<div></div>);
+  }
 }
 
 const DishDetail = props => {
@@ -46,8 +51,23 @@ const DishDetail = props => {
     return (
       <div className="container">
         <div className="row">
-          <RenderDish dish={dish} />
-          <RenderComments comments={dish.comments} />
+                <Breadcrumb>
+                  <BreadcrumbItem>
+                    <Link to='/menu'>Menu</Link>
+                  </BreadcrumbItem>
+                  <BreadcrumbItem active>
+                    {props.dish.name}
+                  </BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                  <h3> {props.dish.name} </h3>
+                  <hr />
+                </div>
+        </div>
+
+        <div className="row">
+          <RenderDish dish={props.dish} />
+          <RenderComments comments={props.comments} />
         </div>
       </div>
     );
